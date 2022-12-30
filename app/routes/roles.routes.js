bb86af2b0ff79742e03d12a6acfbfc5f794f4bcc
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2022-12-30 19:50:00 +0200

    Rules routes added

---
+++ app/routes/roles.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = new express.Router();
+
+router.post("/add")
+router.get("/all")
+router.patch("/update")
+router.delte("/delte")
+
+
+
+module.exports = router;
