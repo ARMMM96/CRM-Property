d5a93d11c75e79a6c1b87646578daef6f29e8147
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2022-12-30 10:00:14 +0200

    Create user method implemented

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -1,3 +1,15 @@
 const userModel = require('../db/models/user.model');
-class User { }
+class User {
+    static register = async (req, res) => {
+        try {
+            const userData = new userModel(req.body)
+            await userData.save()
+            res.josn(userData)
+        }
+        catch (e) {
+            res.josn(e.message)
+        }
+    }
+
+}
 module.exports = User
