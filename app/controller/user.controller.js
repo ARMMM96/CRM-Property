eff90adaad1098d27b59304ca9d6ab362dae61af
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2022-12-30 11:49:31 +0200

    Get single user method added

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -1,7 +1,6 @@
 const userModel = require('../db/models/user.model')
 class User {
     static register = async (req, res) => {
-        console.log("fire?")
         try {
             const userData = new userModel(req.body)
             console.log(req.body)
@@ -14,6 +13,21 @@ class User {
                 apiStatus: false,
                 data: e.message,
                 message: "error adding new user",
+            });
+        }
+    }
+    static getSingleUser = async (req, res) => {
+        try {
+            const userData = await userModel.findOne({
+                _id: req.body.id
+            });
+            res.send(userData)
+        }
+        catch (e) {
+            res.status(500).send({
+                apiStatus: false,
+                data: e.message,
+                message: "error finding user",
             });
         }
     }
