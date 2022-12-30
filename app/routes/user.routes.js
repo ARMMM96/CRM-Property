21138dfc28c829ee4ffbd057dc3290c0f114e801
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2022-12-30 12:20:40 +0200

    Update user route added

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -5,5 +5,6 @@ const userController = require("../controller/user.controller")
 
 router.post("/register", userController.register)
 router.get("/find", userController.getSingleUser)
+router.get("/update", userController.updateUserData)
 
 module.exports = router
