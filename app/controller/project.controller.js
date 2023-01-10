793ad8ddaf01d94c1335d648ba20ffb1f00833d6
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-11 01:27:08 +0200

    Git single project by id method implemented

--- app/controller/project.controller.js
+++ app/controller/project.controller.js
@@ -15,6 +15,15 @@ class Project {
     static getProjects = async (req, res) => {
         try {
             const projectData = await projectModel.find();
+            helper.resHandler(res, 200, true, projectData, "Projects found")
+        }
+        catch (e) {
+            helper.resHandler(res, 500, false, e, e.message)
+        }
+    }
+    static getProject = async (req, res) => {
+        try {
+            const projectData = await projectModel.findById({ _id: req.body.id })
             helper.resHandler(res, 200, true, projectData, "Project found")
         }
         catch (e) {
