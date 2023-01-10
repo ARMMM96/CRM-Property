c64dd26fc12f7248d925058287e77730c91d537a
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-10 22:24:42 +0200

    Email validation added to user model'

---
+++ app/db/models/project.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const projectSchema = mongoose.Schema({
+    Name: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 20,
+        required: true
+    },
+    buildings: {
+        type: Number,
+        min: 1,
+        default: 21
+    },
+    type: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        enum: ['show', 'sell']
+    },
+    location: {
+        type: String,
+        trim: true,
+        required: true
+    }
+})
+
+const Project = mongoose.model('Project', projectSchema)
+
+module.exports = Project
