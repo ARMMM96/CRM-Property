bf4dccef0d197ecb06d0931e71d6c8be5727682d
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-11 01:58:10 +0200

    Buildings model implemented

---
+++ app/db/models/building.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const buildingSchema = mongoose.Schema({
+    number: {
+        type: number,
+        trim: true,
+        required: true
+    },
+    status: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        enum: ['show', 'sell']
+    },
+    location: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    image: {
+        type: String,
+        trim: true
+    },
+    floors: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "floor",
+    }]
+})
+
+const Building = mongoose.model('Building', buildingSchema)
+
+module.exports = Building
