777cc451ee11f25db83a85efd2acd8165d8559d8
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-11 00:37:19 +0200

    Project Routes implemnted

---
+++ app/routes/project.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = new express.Router();
+// const projectController = require("../controller/project.controller")
+
+// router.post("/create", projectController.createRule)
+// router.get("/all", projectController.getRules)
+// router.patch("/update", projectController.updateRules)
+// router.delete("/delete", projectController.deleteRule)
+
+
+
+module.exports = router;
