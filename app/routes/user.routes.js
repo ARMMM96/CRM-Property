625784ecc11015a7e361cc1428e5124201a0bc2c
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-10 23:38:31 +0200

    Login route added to user routes

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -4,6 +4,7 @@ const userController = require("../controller/user.controller")
 
 
 router.post("/register", userController.register)
+router.post("/login", userController.login)
 router.get("/find", userController.getSingleUser)
 router.patch("/update", userController.updateUserData)
 router.delete("/delete", userController.deleteUser)
