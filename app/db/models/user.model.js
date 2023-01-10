4e6dfc51fd26d638f87bd715b2112a0567a6016f
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-10 22:17:34 +0200

    Token property added to user model

--- app/db/models/user.model.js
+++ app/db/models/user.model.js
@@ -1,80 +1,83 @@
-const mongoose = require("mongoose")
-
+const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    firstName: {
+  firstName: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 20,
+    required: true
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 20,
+    required: true
+  },
+  age: {
+    type: Number,
+    min: 21,
+    max: 60,
+    default: 21
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    required: true,
+    unique: true
+  },
+  status: {
+    type: Boolean,
+    default: false
+  },
+  image: {
+    type: String,
+    trim: true
+  },
+  password: {
+    type: String,
+    trim: true,
+    minLength: 5,
+    required: true
+    // match: ''
+  },
+  gender: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    enum: ['male', 'female']
+  },
+  dOfBirth: {
+    type: Date
+  },
+  phoneNum: {
+    type: String
+  },
+  addresses: [
+    {
+      addressType: {
         type: String,
         trim: true,
-        lowercase: true,
-        minLength: 3,
-        maxLength: 20,
         required: true
-    },
-    lastName: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        minLength: 3,
-        maxLength: 20,
-        required: true
-    },
-    age: {
-        type: Number,
-        min: 21,
-        max: 60,
-        default: 21
-    },
-    email: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        required: true,
-        unique: true,
-    },
-    status: {
-        type: Boolean,
-        default: false
-    },
-    image: {
-        type: String,
-        trim: true
-    },
-    password: {
-        type: String,
-        trim: true,
-        minLength: 5,
-        required: true,
-        // match: ''
-    },
-    gender: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        enum: ["male", "female"]
-    },
-    dOfBirth: {
-        type: Date
-    },
-    phoneNum: {
-        type: String,
-    },
-    addresses: [
-        {
-            addressType: {
-                type: String,
-                trim: true,
-                required: true
-            },
-            details: { type: String }
-        }
-    ],
-    role: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Roles",
+      },
+      details: { type: String }
     }
-
+  ],
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Roles'
+  },
+  tokens: [
+    {
+      token: { type: String, required: true }
+    }
+  ]
 })
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
