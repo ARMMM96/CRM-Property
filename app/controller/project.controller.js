99e00679b6930189a8604b79cf6e085c4a53566f
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-11 01:06:19 +0200

    Create project method implemented

--- app/controller/project.controller.js
+++ app/controller/project.controller.js
@@ -2,6 +2,16 @@
 const projectModel = require('../db/models/project.model')
 const helper = require("../helpers/helpers")
 class Project {
+    static createProject = async (req, res) => {
+        try {
+            const projectData = new projectModel(req.body)
+            await projectData.save()
+            helper.resHandler(res, 200, true, projectData, "Project added successfully")
+        }
+        catch (e) {
+            helper.resHandler(res, 500, false, e, e.message)
+        }
+    }
 
 }
 module.exports = Project
