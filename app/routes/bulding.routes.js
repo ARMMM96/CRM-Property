ac7d71335886f61db12676053b3299d9efe9494c
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-11 02:13:42 +0200

    Create bulding route added

--- app/routes/bulding.routes.js
+++ app/routes/bulding.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = new express.Router();
-const buildingController = require("../controller/building.controlle")
+const buildingController = require("../controller/building.controller")
 
+
+router.post("/create", buildingController.createbuilding)
 
 
 
