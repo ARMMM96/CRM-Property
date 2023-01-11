d4b2e4542b88801580752f67d8daf5a420876673
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-11 02:13:01 +0200

    Create building method implemented

--- app/controller/building.controller.js
+++ app/controller/building.controller.js
@@ -2,6 +2,15 @@
 const buildingModel = require('../db/models/building.model')
 const helper = require("../helpers/helpers")
 class Building {
-
+    static createBuilding = async (req, res) => {
+        try {
+            const buildingData = new buildingModel(req.body)
+            await buildingData.save()
+            helper.resHandler(res, 200, true, buildingData, "Building added successfully")
+        }
+        catch (e) {
+            helper.resHandler(res, 500, false, e, e.message)
+        }
+    }
 }
 module.exports = Building
