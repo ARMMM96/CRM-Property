9d827ccaef8e0c9e7efecf674a07a442249162aa
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-12 23:34:06 +0200

    Floor controller updated

--- app/controller/floor.controller.js
+++ app/controller/floor.controller.js
@@ -1,17 +1,18 @@
 
-const buildingModel = require('../db/models/floor.model')
+const floorModel = require('../db/models/floor.model')
 const helper = require("../helpers/helpers")
 class Floor {
     static createFloor = async (req, res) => {
         try {
-            const buildingData = new buildingModel(req.body)
-            await buildingData.save()
-            helper.resHandler(res, 200, true, buildingData, "Floor added successfully")
+            const floorgData = new floorModel(req.body)
+            await floorgData.save()
+            helper.resHandler(res, 200, true, floorgData, "Floor added successfully")
         }
         catch (e) {
             helper.resHandler(res, 500, false, e, e.message)
         }
     }
+
 }
 
 module.exports = Floor
