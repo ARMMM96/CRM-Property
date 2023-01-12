5d3202c65937edb3040147153a0e6b66b2b827b0
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-12 22:33:56 +0200

    Create floor method implmented

--- app/controller/floor.controller.js
+++ app/controller/floor.controller.js
@@ -2,7 +2,16 @@
 const buildingModel = require('../db/models/floor.model')
 const helper = require("../helpers/helpers")
 class Floor {
-
+    static createFloor = async (req, res) => {
+        try {
+            const buildingData = new buildingModel(req.body)
+            await buildingData.save()
+            helper.resHandler(res, 200, true, buildingData, "Floor added successfully")
+        }
+        catch (e) {
+            helper.resHandler(res, 500, false, e, e.message)
+        }
+    }
 }
 
 module.exports = Floor
