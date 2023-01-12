85bca4a25984a7c54ae9ceb0ca3853087874e55b
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-12 23:55:30 +0200

    Delete floor route added to floor routes

--- app/routes/floor.routes.js
+++ app/routes/floor.routes.js
@@ -6,6 +6,7 @@ router.post("/create", floorController.createFloor)
 router.get("/floors", floorController.getFloors)
 router.get("/floor", floorController.getFloor)
 router.patch("/update", floorController.updateFloor)
+router.delete("/delete", floorController.deleteFloor)
 
 
 
