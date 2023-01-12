15a1c58bdc12cf48ace6b5f6631b653d36b9e6ea
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-12 22:10:53 +0200

    Get single building route added

--- app/routes/bulding.routes.js
+++ app/routes/bulding.routes.js
@@ -5,6 +5,7 @@ const buildingController = require("../controller/building.controller")
 
 router.post("/create", buildingController.createBuilding)
 router.get("/buildings", buildingController.getBuildings)
+router.get("/building", buildingController.getBuilding)
 router.patch("/update", buildingController.updateBuilding)
 router.delete("/delete", buildingController.deleteBuilding)
 
