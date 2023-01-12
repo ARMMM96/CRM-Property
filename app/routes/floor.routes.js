436dbf4d71ee3f127d711a137935a89389fe8cb8
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-12 23:48:58 +0200

    Get floor route added to floor routes

--- app/routes/floor.routes.js
+++ app/routes/floor.routes.js
@@ -4,6 +4,7 @@ const floorController = require("../controller/floor.controller")
 
 router.post("/create", floorController.createFloor)
 router.get("/floors", floorController.getFloors)
+router.get("/floor", floorController.getFloor)
 
 
 
