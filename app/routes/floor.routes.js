1043d32f91ad34c4922c31f40cc7e35ac8a8d95c
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-12 23:31:10 +0200

    Create floor route added

--- app/routes/floor.routes.js
+++ app/routes/floor.routes.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = new express.Router();
+const floorController = require("../controller/floor.controller")
+
+router.post("/create", floorController.createFloor)
+
+
+
 
 module.exports = router;
