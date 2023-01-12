5941a479d2a4b3cb5297faff4a03eb83b33a655d
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-12 21:47:29 +0200

    Delet building route added

--- app/routes/bulding.routes.js
+++ app/routes/bulding.routes.js
@@ -6,5 +6,6 @@ const buildingController = require("../controller/building.controller")
 router.post("/create", buildingController.createBuilding)
 router.get("/buildings", buildingController.getBuildings)
 router.patch("/update", buildingController.updateBuilding)
+router.delete("/delete", buildingController.deleteBuilding)
 
 module.exports = router;
