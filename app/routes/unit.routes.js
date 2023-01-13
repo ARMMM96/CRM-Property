fd440006d002033e97164f4c49b959db277a7db8
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-13 11:39:32 +0200

    Get units route added

--- app/routes/unit.routes.js
+++ app/routes/unit.routes.js
@@ -4,6 +4,6 @@ const unitController = require("../controller/unit.controller")
 
 router.post("/create", unitController.createUnit)
 router.patch("/update", unitController.updateUnit)
-
+router.get("/units", unitController.getUnits)
 
 module.exports = router;
