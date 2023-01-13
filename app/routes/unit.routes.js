94f63746bb97e7a8d59356b03a136b01a6d70736
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-13 02:07:08 +0200

    Create unit route added

--- app/routes/unit.routes.js
+++ app/routes/unit.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = new express.Router();
+const unitController = require("../controller/unit.controller")
 
+router.post("/create", unitController.createUnit)
 
 
 module.exports = router;
