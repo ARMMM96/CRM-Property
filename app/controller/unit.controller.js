5f32326f05fa05faac5f2190b64ffd75f0a4ecd6
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-13 02:00:54 +0200

    Create Unit method implemented

--- app/controller/unit.controller.js
+++ app/controller/unit.controller.js
@@ -1,9 +1,18 @@
 
-const floorModel = require('../db/models/unit.model')
+const unitModel = require('../db/models/unit.model')
 const helper = require("../helpers/helpers")
 class Unit {
 
-
+    static createUnit = async (req, res) => {
+        try {
+            const unitgData = new unitModel(req.body)
+            await unitgData.save()
+            helper.resHandler(res, 200, true, unitgData, "Unit added successfully")
+        }
+        catch (e) {
+            helper.resHandler(res, 500, false, e, e.message)
+        }
+    }
 
 }
 
