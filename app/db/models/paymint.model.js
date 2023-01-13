26307f172210fedbe29b43ceb203aee0983ea234
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-13 17:02:19 +0200

    Payming model implemented

---
+++ app/db/models/paymint.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const paymintSchema = mongoose.Schema({
+    totalAmout: {
+        type: Number,
+        trim: true,
+        required: true
+    },
+    unitId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "unit",
+        required: true
+    },
+    customerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
+    sallerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    }
+})
+
+const Paymint = mongoose.model('Paymint', paymintSchema)
+
+module.exports = Floor
