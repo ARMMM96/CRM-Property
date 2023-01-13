8fbc4e5ee228516be3898776b387700d1dbb2343
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-13 11:28:01 +0200

    Update unit method implemetned

--- app/controller/unit.controller.js
+++ app/controller/unit.controller.js
@@ -14,6 +14,21 @@ class Unit {
         }
     }
 
+
+    static updateUnit = async (req, res) => {
+        try {
+            const unitData = await unitModel.findOneAndUpdate(
+                { _id: req.body.id },
+                { ...req.body },
+                { new: true }
+            );
+            helper.resHandler(res, 200, true, unitData, "Unit updated")
+        }
+        catch (e) {
+            helper.resHandler(res, 500, false, e, e.message)
+        }
+    }
+
 }
 
 module.exports = Unit
