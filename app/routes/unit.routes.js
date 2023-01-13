3ef34993ed03095079c7b0fc57a18f850d12f1c5
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-13 11:28:44 +0200

    Update unit route added

--- app/routes/unit.routes.js
+++ app/routes/unit.routes.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const unitController = require("../controller/unit.controller")
 
 router.post("/create", unitController.createUnit)
+router.patch("/update", unitController.updateUnit)
 
 
 module.exports = router;
