6a10655bdbcf975328d917f9c9bc60a066083e33
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-14 06:08:04 +0200

    Update paymint route added

--- app/routes/paymint.routes.js
+++ app/routes/paymint.routes.js
@@ -4,7 +4,7 @@ const paymintController = require("../controller/paymint.controller")
 
 
 router.post("/create", paymintController.createPaymint)
-
+router.patch("/update", paymintController.updatePaymint)
 
 
 module.exports = router;
