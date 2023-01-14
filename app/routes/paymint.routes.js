aeca27040789d1db8d4e37944fd404b1d931a4b1
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-14 05:17:19 +0200

    Create paymint route added to paymint routes

--- app/routes/paymint.routes.js
+++ app/routes/paymint.routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = new express.Router();
+const paymintController = require("../controller/paymint.controller")
 
+
+router.post("/create", paymintController.createPaymint)
 
 
 
