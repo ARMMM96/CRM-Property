58a7f8364691bfdc056c54566f8b4a3cdc67d1e6
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-01-14 05:18:03 +0200

    Create paymint method implemented

--- app/controller/paymint.controller.js
+++ app/controller/paymint.controller.js
@@ -2,8 +2,17 @@
 const paymintModel = require('../db/models/paymint.model')
 const helper = require("../helpers/helpers")
 class Paymint {
+    static createPaymint = async (req, res) => {
+        try {
+            const paymintgData = new paymintModel(req.body)
+            await paymintgData.save()
+            helper.resHandler(res, 200, true, paymintgData, "Paymint added successfully")
+        }
+        catch (e) {
+            helper.resHandler(res, 500, false, e, e.message)
+        }
+    }
 
- 
 }
 
 module.exports = Paymint
